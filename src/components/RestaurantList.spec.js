638555a968a9b1afc227cfbc2e51feda3ef4a935
Christof Kälin <christof.kaelin@protonmail.com>
2024-02-26 20:33:42 +0100

    Refactor RestaurantList component to display a list of restaurants

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,13 +1,33 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
   it('loads restaurants on first render', () => {
     // Arrange
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    const restaurants = [];
     // Act
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
     // Assert
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    // Arrange
+    const noop = () => {};
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+    // Act
+    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+    // Assert
+    expect(screen.getByText('Sushi Place')).toBeInTheDocument();
+    expect(screen.getByText('Pizza Place')).toBeInTheDocument();
   });
 });
