fbbdec8ef0c01b7ab9480418a6fbec5ff49255b8
Christof Kälin <christof.kaelin@protonmail.com>
2024-02-26 20:38:19 +0100

    Refactor RestaurantList.spec.js to improve code structure and readability

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -2,17 +2,26 @@ import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    // Arrange
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
-    // Act
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+  let loadRestaurants;
+
+  function renderComponent() {
     render(
       <RestaurantList
         loadRestaurants={loadRestaurants}
         restaurants={restaurants}
       />,
     );
+  }
+
+  it('loads restaurants on first render', () => {
+    // Arrange
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+    // Act
+    renderComponent();
     // Assert
     expect(loadRestaurants).toHaveBeenCalled();
   });
@@ -20,13 +29,8 @@ describe('RestaurantList', () => {
   it('displays the restaurants', () => {
     // Arrange
     const noop = () => {};
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
     // Act
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
-    // Assert
+    renderComponent(); // Assert
     expect(screen.getByText('Sushi Place')).toBeInTheDocument();
     expect(screen.getByText('Pizza Place')).toBeInTheDocument();
   });
